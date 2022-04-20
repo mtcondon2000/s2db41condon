8659cbad7a61d77144ee89b89dc804fad5ebfa85
mtcondon2000 <55842488+mtcondon2000@users.noreply.github.com>
2022-04-20 13:49:52 -0500

    changes

--- routes/teslas.js
+++ routes/teslas.js
@@ -7,12 +7,12 @@ router.get('/', tesla_controller.tesla_view_all_Page);
 
 router.get('/teslas', tesla_controller.tesla_list); 
 
-router.get('/detail/?id', tesla_controller.tesla_view_one_Page);
+router.get('/detail', tesla_controller.tesla_view_one_Page);
  
 router.get('/create', tesla_controller.tesla_create_Page); 
 
-router.get('/update/?id', tesla_controller.tesla_update_Page); 
+router.get('/update', tesla_controller.tesla_update_Page); 
 
-router.get('/delete/?id', tesla_controller.tesla_delete_Page); 
+router.get('/delete', tesla_controller.tesla_delete_Page); 
 
 module.exports = router;
